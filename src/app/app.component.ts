03ed599b75aff1ac6789f8f803d09148b9b8d615
Fernando del olmo martin <fer.olmo92@gmail.com>
2017-06-05 13:17:21 +0200

    Removed sidemenu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,23 @@
-import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { SettingsPage } from '../pages/settings/settings';
+
 @Component({
   templateUrl: 'app.html'
 })
-export class MyApp {
-  @ViewChild(Nav) nav: Nav;
-  
+export class MyApp {  
   rootPage:any = HomePage;
-  pages: Array<{ title: string, component: any }>;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
-
-        // Set sidemenu pages
-        this.pages = [
-          {
-            title: 'home', component: HomePage
-          },
-          {
-            title: 'settings', component: SettingsPage
-          }
-        ];
-
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
-  }
-
-  openPage(page) {
-    this.nav.setRoot(page.component);
   }
 }
 
