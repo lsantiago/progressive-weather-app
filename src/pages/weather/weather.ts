5c4e2802332b7ede8aa4133cc8be1f79d03817f2
Fernando del olmo martin <fer.olmo92@gmail.com>
2017-08-11 11:34:18 +0200

    Minor changes

--- src/pages/weather/weather.ts
+++ src/pages/weather/weather.ts
@@ -10,28 +10,24 @@ import { WeatherService } from '../../services/weather.service';
 export class WeatherPage implements OnInit{
 
   current;
+  astro;
   location;
   forecast;
   today;
-  moonphases;
 
   constructor(private weather: WeatherService,
               private navParams: NavParams) {}
 
   ngOnInit() {
     let place = this.navParams.get('place');
-
-    this.weather.weather(place).subscribe(data => {
-      this.current = data.current;
-      this.location = data.location;
-    });
-
     this.weather.forecast(place, 7).subscribe(data => {
-      this.forecast = data.forecast.forecastday;
-      this.today = this.forecast[0];
-      this.current.max_c = this.forecast[0].day.maxtemp_c;
-      this.current.min_c = this.forecast[0].day.mintemp_c;
-      this.current.astro = this.forecast[0].astro;
+      this.current       = data.current;
+      this.location      = data.location;
+      this.forecast      = data.forecast.forecastday;
+      this.today         = data.forecast.forecastday[0];
+      this.astro         = data.forecast.forecastday[0].astro;
+      this.current.max_c = data.forecast.forecastday[0].day.maxtemp_c;
+      this.current.min_c = data.forecast.forecastday[0].day.mintemp_c;
     });
   }
 }
